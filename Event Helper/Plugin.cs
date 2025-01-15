2c41eb11993bd158d149c2ee0bd0314637723e1e
SCP-207 <davideby09@gmail.com>
2025-01-14 20:32:55 -0700

    Forgot to increase plugin version

--- Event Helper/Plugin.cs
+++ Event Helper/Plugin.cs
@@ -13,7 +13,7 @@ namespace Event_Helper {
         public override string Prefix { get; } = "EH";
         public override PluginPriority Priority { get; } = PluginPriority.Default;
         public override Version RequiredExiledVersion { get; } = new(9, 3, 0);
-        public override Version Version { get; } = new(3, 5, 0);
+        public override Version Version { get; } = new(3, 5, 1);
 
         public static Plugin Instance { get; private set; }
 
