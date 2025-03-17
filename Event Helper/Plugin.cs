23e847a6769703b417651e3d3c77c25969eb8736
SCP-207 <davideby09@gmail.com>
2025-03-17 14:52:58 -0700

    Updated versions (forgot to do that)

--- Event Helper/Plugin.cs
+++ Event Helper/Plugin.cs
@@ -12,8 +12,8 @@ namespace Event_Helper {
         public override string Name { get; } = "Event Helper";
         public override string Prefix { get; } = "EH";
         public override PluginPriority Priority { get; } = PluginPriority.Default;
-        public override Version RequiredExiledVersion { get; } = new(9, 4, 0);
-        public override Version Version { get; } = new(3, 5, 2);
+        public override Version RequiredExiledVersion { get; } = new(9, 5, 1);
+        public override Version Version { get; } = new(3, 5, 3);
 
         public static Plugin Instance { get; private set; }
 
